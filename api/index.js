cf391155b03881008829e34f889ba2583d0d339d
Deez Nutz <jadalouk@gmail.com>
2024-12-20 01:06:27 +0200

    database connection

--- api/index.js
+++ api/index.js
@@ -1,4 +1,11 @@
 import express from "express";
+import mongoose from "mongoose";
+import dotenv from "dotenv"
+
+dotenv.config()
+mongoose.connect(process.env.MONGO_URI)
+        .then(() => console.log("Connected To DB..."))
+        .catch((err) => console.log(err))
 
 const app = express()
 
