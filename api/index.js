0bc36ce17e9a07dab7b82e0612786e1b1ff6e03b
Deez Nutz <jadalouk@gmail.com>
2024-12-20 02:37:50 +0200

    created sign up api route

--- api/index.js
+++ api/index.js
@@ -1,6 +1,9 @@
 import express from "express";
 import mongoose from "mongoose";
 import dotenv from "dotenv"
+import userRouter from "./routes/user.routes.js"
+import authRouter from "./routes/auth.routes.js"
+
 
 dotenv.config()
 mongoose.connect(process.env.MONGO_URI)
@@ -8,7 +11,10 @@ mongoose.connect(process.env.MONGO_URI)
         .catch((err) => console.log(err))
 
 const app = express()
+app.use(express.json())
 
+app.use('/api/user', userRouter)
+app.use('/api/auth', authRouter)
 
 app.listen(3000, () => {
     console.log(`Server is listening on port 3000...`)
