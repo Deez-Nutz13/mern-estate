466fb4ff8ad3652a31f76e0a0705e9cfa8d44277
Deez Nutz <jadalouk@gmail.com>
2024-12-21 18:29:22 +0200

    goofy

--- api/index.js
+++ api/index.js
@@ -1,31 +1,39 @@
-import express from "express";
-import mongoose from "mongoose";
-import dotenv from "dotenv"
-import userRouter from "./routes/user.routes.js"
-import authRouter from "./routes/auth.routes.js"
+import express from 'express';
+import mongoose from 'mongoose';
+import dotenv from 'dotenv';
+import userRouter from './routes/user.routes.js';
+import authRouter from './routes/auth.routes.js';
 
+dotenv.config();
 
-dotenv.config()
-mongoose.connect(process.env.MONGO_URI)
-        .then(() => console.log("Connected To DB..."))
-        .catch((err) => console.log(err))
-
-const app = express()
-app.use(express.json())
-
-app.use('/api/user', userRouter)
-app.use('/api/auth', authRouter)
-
-app.use((err, req, res, next) => {
-    const statusCode = err.statusCode || 500;
-    const message = err.message || 'Internal Server Error';
-    return res.status(statusCode).json({
-      success: false,
-      statusCode,
-      message,
-    });
+mongoose
+  .connect(process.env.MONGO_URI)
+  .then(() => {
+    console.log('Connected to MongoDB!');
+  })
+  .catch((err) => {
+    console.log(err);
   });
 
+const app = express();
+
+app.use(express.json());
+
+
 app.listen(3000, () => {
-    console.log(`Server is listening on port 3000...`)
-})
+  console.log('Server is running on port 3000!');
+});
+
+app.use('/api/user', userRouter);
+app.use('/api/auth', authRouter);
+
+
+app.use((err, req, res, next) => {
+  const statusCode = err.statusCode || 500;
+  const message = err.message || 'Internal Server Error';
+  return res.status(statusCode).json({
+    success: false,
+    statusCode,
+    message,
+  });
+});
--- api/models/user.models.js
+++ api/models/user.models.js
@@ -1,26 +1,29 @@
-import mongoose from "mongoose";
+import mongoose from 'mongoose';
 
-const userSchema = new mongoose.Schema({
+const userSchema = new mongoose.Schema(
+  {
     username: {
-        type: String,
-        required: true,
-        unique: true
+      type: String,
+      required: true,
+      unique: true,
     },
     email: {
-        type: String,
-        required: true,
-        unique: true
+      type: String,
+      required: true,
+      unique: true,
     },
     password: {
-        type: String,
-        required: true,
+      type: String,
+      required: true,
     },
-    avatar: {
-        type: String,
-        default: "https://media.istockphoto.com/id/2151669184/vector/vector-flat-illustration-in-grayscale-avatar-user-profile-person-icon-gender-neutral.jpg?s=612x612&w=0&k=20&c=UEa7oHoOL30ynvmJzSCIPrwwopJdfqzBs0q69ezQoM8="
-    }
-}, {timestamps: true})
+    avatar:{
+      type: String,
+      default: "https://cdn.pixabay.com/photo/2015/10/05/22/37/blank-profile-picture-973460_1280.png"
+    },
+  },
+  { timestamps: true }
+);
 
-const User = mongoose.model('User', userSchema)
+const User = mongoose.model('User', userSchema);
 
-export default User
+export default User;
