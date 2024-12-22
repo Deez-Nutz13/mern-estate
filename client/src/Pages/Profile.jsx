d7ba9e49fa32c8b45d32a1702b1b4f0628a94d03
Deez Nutz <jadalouk@gmail.com>
2024-12-22 23:51:03 +0200

    delete user api and functionality

--- client/src/Pages/Profile.jsx
+++ client/src/Pages/Profile.jsx
@@ -1,7 +1,14 @@
 import { useSelector} from "react-redux"
 import { useRef, useState } from "react"
-import {updateUserStart, updateUserSuccess, updateUserFailure} from "../redux/user/userSlice.js"
 import { useDispatch } from "react-redux"
+import {
+    updateUserStart, 
+    updateUserSuccess, 
+    updateUserFailure,
+    deleteUserStart,
+    deleteUserSuccess,
+    deleteUserFailure
+} from "../redux/user/userSlice.js"
 
 export default function Profile() {
 
@@ -41,6 +48,23 @@ export default function Profile() {
         }
     }
 
+    const handleDeleteUser = async () => {
+        try {
+            dispatch(deleteUserStart())
+            const res = await fetch(`/api/user/delete/${currentUser._id}`, {
+                method: "DELETE"
+            })
+            const data = await res.json()
+            if (data.success === false) {
+                dispatch(deleteUserFailure(data.message))
+                return
+            }
+            dispatch(deleteUserSuccess(data))
+        } catch (error) {
+            dispatch(deleteUserFailure(error.message))
+        }
+    }
+
     return (
         <div className="p-3 max-w-lg mx-auto">
             <h1 className="text-3xl front-semibold text-center my-7">Profile</h1>
@@ -68,7 +92,7 @@ export default function Profile() {
                 </button>
             </form>
             <div className="flex justify-between mt-5">
-                <span className="text-red-700 cursor-pointer ">Delete Account</span>
+                <span onClick={handleDeleteUser} className="text-red-700 cursor-pointer ">Delete Account</span>
                 <span className="text-red-700 cursor-pointer ">Sign Out</span>
             </div>
             <p className="text-red-700 mt-5">{error ? error : ''}</p>
